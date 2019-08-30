ec1a56b0e37ebb3ea18523a0705634ded4445075
GIOVANNI ROSA DA SILVA <grs14@urquell>
2019-08-30 18:04:43 -0300

    rsa working

--- rsa.c
+++ rsa.c
@@ -13,13 +13,27 @@ void printBN(char *msg, BIGNUM *a) {
 
 BIGNUM *get_private_key(BIGNUM *p, BIGNUM *q, BIGNUM *e, BIGNUM *n,
                         BN_CTX *ctx) {
-  BIGNUM *d = BN_dup(n);
-  BN_sub(d, d, p);
-  BN_sub(d, d, q);
-  BN_add_word(d, 1);
-  BN_mod_inverse(d, e, d, ctx);
+  BIGNUM *aux = BN_new();
+  BIGNUM *p_1 = BN_new();
+  BIGNUM *q_1 = BN_new();
+  BIGNUM *two = BN_new();
 
-  return d;
+  // phi_n = (p - 1) * (q - 1)
+  BN_sub(p_1, p, BN_value_one());
+  BN_sub(q_1, q, BN_value_one());
+  BN_mul(aux, p_1, q_1, ctx);
+
+  // d = ((2 * phi_n) + 1) / e
+  BN_dec2bn(&two, "2");
+  BN_mul(aux, aux, two, ctx);
+  BN_add(aux, aux, BN_value_one());
+  BN_div(aux, NULL, aux, e, ctx);
+  
+  BN_free(q_1);
+  BN_free(p_1);
+  BN_free(two);
+
+  return aux;
 }
 
 int main() {
@@ -28,7 +42,7 @@ int main() {
   BIGNUM *q = BN_new();
   BIGNUM *e = BN_new();
   BIGNUM *n = BN_new();
-  BIGNUM *m = BN_new();
+  BIGNUM *m = BN_new();//BN_value_one
   BIGNUM *c = BN_new();
 
   // Assign a value from a hex number string
